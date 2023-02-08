6ded033ef9a10ecdfa26b8f22f45dd2c8fcee5a9
Sujeet <svontheweb@gmail.com>
2023-02-08 15:00:19 -0500

    enabled javascript with WebSettings

--- app/src/main/java/com/example/javawebview/MainActivity.java
+++ app/src/main/java/com/example/javawebview/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.javawebview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
@@ -17,6 +18,9 @@ public class MainActivity extends AppCompatActivity {
         webView = (WebView) findViewById(R.id.webview);
         webView.setWebViewClient(new WebViewClient());
         webView.loadUrl("https://www.google.com");
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
     }
 
     @Override
