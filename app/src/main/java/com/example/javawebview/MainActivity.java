14059e967ca38dd544f811f5950960207cd0d558
Sujeet <svontheweb@gmail.com>
2023-02-08 14:14:30 -0500

    implemented in mainactivity
    back button exits the app

--- app/src/main/java/com/example/javawebview/MainActivity.java
+++ app/src/main/java/com/example/javawebview/MainActivity.java
@@ -3,12 +3,19 @@ package com.example.javawebview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
+    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        webView = (WebView) findViewById(R.id.webview);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://www.google.com");
     }
 }
